f74a0220c6e59d86d03908328b320e68e2f5dc7e
Maria Vitória <mariavitoria6019@gmail.com>
2025-11-27 11:11:50 -0300

    build: creat the first function

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs-extra');
+const PDFDocument = require('pdfkit');
 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+ipcMain.handle('select-txt', async () => {
+  const result = await dialog.showOpenDialog({
+    filters: [{ name: 'Text Files', extensions: ['txt'] }],
+    properties: ['openFile']
+  });
+  return result.filePaths[0];
+});
